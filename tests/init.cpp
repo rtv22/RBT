80da7a0d601551e8c64642565b6274e6eeb09339
rtv22 <rtvcojigor@gmail.com>
2017-05-28 20:20:29 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -11,7 +11,7 @@ SCENARIO("insert", "[init]")
 {
   RBT<int> test;
   test.insert(1);
-  REQUIRE(test.search(1) != 0);
+  REQUIRE(test.findNode(1) != 0);
 }
 
 SCENARIO("insert_1", "[init]")
@@ -20,11 +20,11 @@ SCENARIO("insert_1", "[init]")
   tree.insert(1);
   tree.insert(2);
   tree.insert(3);
-  REQUIRE(tree._root() == tree.search(2));
+  REQUIRE(tree._root() == tree.findNode(2));
   REQUIRE(tree._color(2) == 1);
   REQUIRE(tree._color(1) == 0);
   REQUIRE(tree._color(3) == 0);
-  REQUIRE(tree.search(1) != 0);
-  REQUIRE(tree.search(2) != 0);
-  REQUIRE(tree.search(3) != 0);
+  REQUIRE(tree.findNode(1) != 0);
+  REQUIRE(tree.findNode(2) != 0);
+  REQUIRE(tree.findNode(3) != 0);
 }
