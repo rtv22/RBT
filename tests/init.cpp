d52554a5560c46c361df2acee8fc92447eec4f7c
rtv22 <rtvcojigor@gmail.com>
2017-05-28 17:58:44 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,8 +12,8 @@ SCENARIO("findNode&insert", "[find_node]")
     RBT<int> obj;
     obj.insert(4);
     obj.insert(2);
-    REQUIRE(obj.findNode(2) == 1);
-    REQUIRE(obj.findNode(5) ==0)
+    REQUIRE(obj.findNode(2) != 0);
+    REQUIRE(obj.findNode(5) == 0);
   }
   
   SCENARIO("Color_check", "[color]")
@@ -22,7 +22,7 @@ SCENARIO("findNode&insert", "[find_node]")
  	 obj.insert(1);           						//const bool RED = 0;
  	 obj.insert(2);
  	 obj.insert(3);
-  	REQUIRE(obj._color(2) == 1);
+  	REQUIRE(obj._color(2) != 0);
   	REQUIRE(obj._color(1) == 0);
   	REQUIRE(obj._color(3) == 0);
   }
