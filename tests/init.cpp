8004aafb07b540279b708f876e4176af01d3846b
rtv22 <rtvcojigor@gmail.com>
2017-05-28 17:55:21 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,8 +3,8 @@
 
 SCENARIO ("init", "[init]")
 {
-  RBT<int> obj;
-  REQUIRE(obj._root()==obj._NIL());
+	RBT<int> obj;
+	REQUIRE(obj._root()==obj._NIL());
 }
 
 SCENARIO("findNode&insert", "[find_node]")
@@ -13,16 +13,16 @@ SCENARIO("findNode&insert", "[find_node]")
     obj.insert(4);
     obj.insert(2);
     REQUIRE(obj.findNode(2) == 1);
-	REQUIRE(obj.findNode(5) ==0)
+    REQUIRE(obj.findNode(5) ==0)
   }
   
   SCENARIO("Color_check", "[color]")
-{
-  RedBlackTree<int> obj; 						//const bool BLACK = 1
-  obj.insert(1);           						//const bool RED = 0;
-  obj.insert(2);
-  obj.insert(3);
-  REQUIRE(obj._color(2) == 1);
-  REQUIRE(obj._color(1) == 0);
-  REQUIRE(obj._color(3) == 0);
+  {
+  	RBT<int> obj; 							//const bool BLACK = 1
+ 	 obj.insert(1);           						//const bool RED = 0;
+ 	 obj.insert(2);
+ 	 obj.insert(3);
+  	REQUIRE(obj._color(2) == 1);
+  	REQUIRE(obj._color(1) == 0);
+  	REQUIRE(obj._color(3) == 0);
   }
